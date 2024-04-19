caafc1dbb2e6b44ea7cc4ff2be11b1d557e195b3
Mohammed Abed Alkareem <mohammad.2003.abedalkareem@gmail.com>
2024-04-19 14:43:50 +0200

    attempt to do the cross-over byt still have a problem that the if statement not working

---
+++ genetic_algorithm.py
@@ -0,0 +1,233 @@
+import copy
+import random
+import time
+
+import matplotlib.pyplot as plt
+
+from matplotlib.patches import Patch
+
+from projectFiles.Job_Phases import *
+
+random.seed(time.time())
+
+
+def create_chromosome(Jobs, population_size = 7):
+
+    population = set()
+    x = 0
+
+    while len(population) < population_size:
+
+        x += 1 # count the number of iterations
+
+        chromosome = []
+        Jobs_copy = copy.deepcopy(Jobs)  # Create a copy of Jobs
+
+        job_phase_order = {}
+
+        for job in Jobs.keys():
+            job_phase_order[job] = 1
+
+        while Jobs_copy:
+
+            job = random.choice(list(Jobs_copy.keys()))
+            if Jobs_copy[job]:
+                phase = Jobs_copy[job].pop(0)
+                # chromosome.append((job, phase))
+
+                chromosome.append(
+                    Phases(
+                        job=job,
+                        phase_order=job_phase_order[job],
+                        machine=phase[0],
+                        duration=phase[1]
+                    )
+                )
+
+                job_phase_order[job] += 1
+            else:
+                del Jobs_copy[job]
+
+        # Convert the chromosome to a tuple before adding to the set
+        population.add(tuple(chromosome))
+
+    # Convert the set back to a list for consistency with the rest of your code
+    population = list(population)
+
+    # print("Population")
+    # for chromosome in population:
+    #     # print(chromosome)
+    #     # print(len(chromosome))
+    #     print("_____________________________________________________")
+    #     for phase in chromosome:
+    #         print(phase.__repr__())
+    #
+    # print(x)
+    return population
+
+
+def machine_phases(chromosome):
+
+    machines_process = {}  #the dictionary for machines phase order
+    jobs = {}  # to store weather if a job is currently working
+
+    for phase in chromosome:
+        machines_process[phase.machine] = {'process': [], 'finish_time': 0}
+        jobs[phase.job] = {'available': True, 'order': 1, 'available_at': 0}
+
+
+    #sort machines_process based on the number after M
+    machines_process = dict(sorted(machines_process.items(), key=lambda x: int(x[0][1:])))
+
+    # print(machines_process)
+
+    jobs = dict(sorted(jobs.items()))
+
+    chrom = list(chromosome)
+
+    print(chrom)
+
+    t = 0
+    while True:
+        # print("Time: ", t)
+        for phase in chrom:
+            if jobs[phase.job]['available'] and jobs[phase.job]['order'] == phase.phase_order and machines_process[phase.machine]['finish_time'] <= t:
+                # print("Job: ", phase.job, "Machine: ", phase.machine, "Duration: ", phase.duration, "Start Time: ", t)
+                jobs[phase.job]['available'] = False
+                jobs[phase.job]['available_at'] = phase.duration + t - 1  # Adjust here
+
+                machines_process[phase.machine]['finish_time'] = phase.duration + t
+
+                machines_process[phase.machine]['process'].append(
+                    ProcessPhases(job=phase.job,
+                                  phase_order=jobs[phase.job]['order'],
+                                  machine=phase.machine,
+                                  duration=phase.duration,
+                                  start_time=t
+                                  )
+                )
+
+                jobs[phase.job]['order'] += 1
+
+                chrom.remove(phase)
+
+        for phase in chrom:
+            if jobs[phase.job]['available_at'] == t:
+                # print("yes")
+                jobs[phase.job]['available'] = True
+
+        if not chrom:
+            break
+
+        t += 1
+
+    print("machines_process")
+    for machine in machines_process:
+        print(machine, machines_process[machine])
+
+    # print("jobs")
+    # for job in jobs:
+    #     print(job, jobs[job])
+
+    return machines_process
+
+
+def draw_gantt_chart(machines_process, jobs):
+
+    fig, ax = plt.subplots(figsize=(12, 8))
+
+    yticks = []
+    yticklabels = []
+
+    colors = plt.cm.tab20.colors
+
+    job_colors = {job: colors[i % len(colors)] for i, job in enumerate(jobs)}
+
+    for i, machine in enumerate(machines_process):
+        yticks.append(i)
+        yticklabels.append(machine)
+        for phase in machines_process[machine]['process']:
+            ax.broken_barh([(phase.start_time, phase.duration)], (i - 0.4, 0.8), facecolors=job_colors[phase.job])
+
+    ax.set_yticks(yticks)
+    ax.set_yticklabels(yticklabels)
+    ax.set_xlabel("Time", fontsize=18)
+    ax.set_ylabel("Machine", fontsize=18)
+    ax.set_title("Gantt Chart", fontsize=22)
+
+
+    # make the x axis more divided
+    ax.set_xticks(range(0, max([phase.start_time + phase.duration for machine in machines_process for phase in machines_process[machine]['process']]), 2))
+
+
+
+    legend_patches = [Patch(color=color, label=job) for job, color in job_colors.items()]
+
+    # Adjust the legend box
+    ax.legend(handles=legend_patches, bbox_to_anchor=(1, 1), loc='upper left', prop={'size': 12}, fancybox=True, shadow=True, edgecolor='black', facecolor='white', framealpha=0.7)
+
+    #start from 0
+    ax.set_xlim(0, max([phase.start_time + phase.duration for machine in machines_process for phase in machines_process[machine]['process']]))
+
+    plt.show()
+
+
+def extract_jobs(machines_process):
+    jobs = set()
+    for machine_data in machines_process.values():
+        for process_phase in machine_data['process']:
+            jobs.add(process_phase.job)
+
+    #sort the set based on the number after _
+    jobs = sorted(jobs, key=lambda x: int(x.split("_")[1]))
+
+    return jobs
+
+
+def get_makespan(machines_process):  #this function is to get the makespan wich is the fitness function
+    makespan = 0
+    for machine_data in machines_process.values():
+        makespan = max(makespan, machine_data['finish_time'])
+    return makespan
+
+def make_crossover(parent1, parent2):
+    child1 = []
+    child2 = []
+
+    # Get the length of the chromosome
+    chromosome_length = len(parent1)
+    #get two random number less than the length of the chromosome TO MAKE THE CUTT
+    cut1 = random.randint(1, chromosome_length - 2)
+    cut2 = random.randint(cut1+1, chromosome_length - 1)
+
+    print('length of chromosome: ', chromosome_length)
+
+    print("Cut1: ", cut1)
+    print("Cut2: ", cut2)
+
+    child1 = list(parent1[:cut1] + parent1[cut2:])
+    #then fill the rest in between cut 1 and cut 2 from parent2 to child1 if not in child 1
+
+    print(type(child1))
+    print(type(parent2))
+
+    for phase in parent2:
+        if phase not in child1:  #there is a problem here
+            #append after cut1 and before cut2
+            child1.insert(cut1, phase)
+            cut1 += 1
+
+
+    print("Child1")
+    for phase in child1:
+        print(phase.__repr__())
+
+    print("chromosome 1" )
+    for phase in parent1:
+        print(phase.__repr__())
+
+    print("chromosome 2")
+    for phase in parent2:
+        print(phase.__repr__())
+
+    draw_gantt_chart(machine_phases(child1), extract_jobs(machine_phases(child1)))
--- projectFiles/Job_Phases.py
+++ projectFiles/Job_Phases.py
@@ -9,6 +9,11 @@ class Phases:
     def __repr__(self):
         return f"{self.job}, order {self.phase_order} , machine {self.machine}, duration {self.duration}"
 
+    # def __eq__(self, other):
+    #     if isinstance(other, Phases):
+    #         return self.job == other.job and self.phase_order == other.phase_order and self.machine == other.machine and self.duration == other.duration
+    #     return False
+
 
 class ProcessPhases (Phases):
     def __init__(self, job, phase_order, machine, duration, start_time):
@@ -17,3 +22,5 @@ class ProcessPhases (Phases):
 
     def __repr__(self):
         return super().__repr__() + f" start time {self.start_time}"
+
+
