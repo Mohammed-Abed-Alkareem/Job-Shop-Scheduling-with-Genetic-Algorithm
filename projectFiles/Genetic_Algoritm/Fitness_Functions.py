068bde7a5caba78c2ccf123d5c577848b39b8191
Mohammed Abed Alkareem <mohammad.2003.abedalkareem@gmail.com>
2024-05-06 13:55:18 +0300

    this fixes #9

--- projectFiles/Genetic_Algoritm/Fitness_Functions.py
+++ projectFiles/Genetic_Algoritm/Fitness_Functions.py
@@ -8,7 +8,7 @@ def get_makespan(machines_process):  #this function is to get the makespan wich 
 
     return makespan
 
-def get_working_time(machines_process):  #this function is to get the makespan wich is the fitness function
+def get_working_time(machines_process):  #this function is to get the makespan which is the fitness function
     '''This function is to get the working time of the machines'''
     #summation of all machines makespan - start time
     working_time = 0
@@ -16,3 +16,12 @@ def get_working_time(machines_process):  #this function is to get the makespan w
         working_time += machine_data['finish_time'] - machine_data['start_time']
 
     return working_time
+
+def combination_fitnesses(*args):  # just a sample
+    '''This function is to get the combination of the fitness functions'''
+    #set weights for each fitness function
+    weights = [1 / len(args) for _ in args]
+    #get the fitness value for each function
+    fitness_values = [weight * fitness_function for weight, fitness_function in zip(weights, args)]
+    #return the sum of the fitness values
+    return sum(fitness_values)
